524fbd0b9582fc40b39d8414da6f2514bfa21b8e
unknown <Administrator@KLESHOSP>
2026-03-30 17:37:30 +0530

    song cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import './App.css';
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import Navbar from './Component/Navbar/Navbar';
 import Hero from './Component/Hero/Hero';
+import Cards from './Component/Card/Card';
+// import Grid from '@mui/material/Grid';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Typography ,Box,Grid,Button} from '@mui/material';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://qtify-backend.labs.crio.do/albums/top');
+        const ApiData = response.data;
+        setData(ApiData);
+        // console.log();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+
   return (
     <Box container className="App">
-
+      {/* {console.log(data)} */}
       <Navbar  />
       <Hero />
+
+      <Grid container spacing={1} sx={{display: 'flex', justifyContent:'space-between'}}>
+
+          <Typography variant="h6" sx={{ color: '#fff', padding: '5px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontWeight: 'bold'}}>
+            Top Albums
+          </Typography>  
+          <Button variant="text">See All</Button>
+        
+
+        <Grid container spacing={1}>
+          {data.map((item, index) => (
+            <Cards key={index} item={item} />
+          ))}
+        </Grid>
+      </Grid>
     </Box>
   );
 }
---
+++ src/Component/Card/Card.jsx
@@ -0,0 +1,31 @@
+import style from "./Card.module.css";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+// import { Box } from "@mui/material";
+import Chip from '@mui/material/Chip';
+import Grid from '@mui/material/Grid';
+
+export default function Cards({ item }) {
+    console.log(item);
+  return (
+    <Grid size={{xs: 6, sm: 4, md:3 ,lg: 1.7}} container spacing={1} sx={{ marginTop: '20px', padding: '0px 20px', display: 'flex', justifyContent: 'center', alignContent:'center', alignItems: 'flex-start', flexDirection: 'column'}}>
+        <Card xs={{xs: 12, sm: 6, md: 4, lg: 3}} sx={{width: '159px', height:'205px', borderRadius: '15px', boxShadow: '0px 0px 10px rgba(255, 255, 255, 0.51)'}}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="170px"
+                    image={item.image}
+                    alt="green iguana"
+                />
+                <CardContent sx={{ display: 'flex', alignItems: 'center',  padding: '5px'}}>
+                    <Chip sx={{ bgcolor: '#121212', color: '#ffffffff', fontSize: '10px', width:'fit-content', height:'23px'}} label={`${item.follows} Follows`} />
+                </CardContent>
+            </CardActionArea>
+        </Card>
+        <p style={{ color: '#fff'}}>{item.title}</p>
+  </Grid>
+);
+}
