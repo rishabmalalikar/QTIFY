29ff062e8e705b29533830c539115dde0989db70
rishab <rishabmalalikar@gmail.com>
2026-03-30 23:21:19 +0530

    Top and Albums

--- src/App.js
+++ src/App.js
@@ -7,45 +7,25 @@ import Cards from './Component/Card/Card';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Typography ,Box,Grid,Button} from '@mui/material';
+import NewAlbum from './Component/NewAlbums/NewAlbums';
+import TopAlbum from './Component/TopAlbums/TopAlbums';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get('https://qtify-backend.labs.crio.do/albums/top');
-        const ApiData = response.data;
-        setData(ApiData);
-        // console.log();
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-
-    fetchData();
-  }, []);
 
 
   return (
     <Box container className="App">
-      {/* {console.log(data)} */}
       <Navbar  />
       <Hero />
 
-      <Grid container spacing={1} sx={{display: 'flex', justifyContent:'space-between'}}>
-
-          <Typography variant="h6" sx={{ color: '#fff', padding: '5px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontWeight: 'bold'}}>
-            Top Albums
-          </Typography>  
-          <Button variant="text">See All</Button>
+      <Grid container spacing={1} sx={{display: 'flex', justifyContent:'space-between'}} px={2}>
+      <TopAlbum TopAlbum={"Top Albums"}/>
         
+      </Grid>
 
-        <Grid container spacing={1}>
-          {data.map((item, index) => (
-            <Cards key={index} item={item} />
-          ))}
-        </Grid>
+      <Grid container spacing={1} sx={{display: 'flex', justifyContent:'space-between'}} px={2}>
+        
+      <NewAlbum newAlbum={"New Albums"}/>
       </Grid>
     </Box>
   );
---
+++ src/Component/NewAlbums/NewAlbums.jsx
@@ -0,0 +1,41 @@
+import Cards from '../Card/Card';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Typography ,Box,Grid,Button} from '@mui/material';
+
+
+export default function NewAlbum(newAlbum){
+    const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://qtify-backend.labs.crio.do/albums/top');
+        const ApiData = response.data;
+        setData(ApiData);
+        // console.log();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+
+  return (
+    
+
+      <>
+          <Typography variant="h6"  sx={{ color: '#fff', padding: '5px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontWeight: 'bold'}}>
+            {newAlbum.newAlbum}
+          </Typography>  
+          <Button variant="text">See All</Button>
+        <Grid container spacing={1}>
+          {data.map((item, index) => (
+            <Cards key={index} item={item} />
+          ))}
+        </Grid>
+      </>
+  );
+}
